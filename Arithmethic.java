74f236bd9b8422ad4b9b5ace23a03f70aeaff105
SobamboS <sobamboolusola1152@gmail.com>
2022-07-31 11:29:05 +0100

    renamed

--- Arithmethic.java
+++ Arithmethic.java
@@ -7,23 +7,23 @@ public class Arithmethic {  // class
 
         Scanner input = new Scanner(System.in);   // create a scanner to obtain input from the user
 
-        System.out.print("Enter first number : ");   // prompt user to input first number
+        System.out.print("Enter first number is : ");   // prompt user to input first number
         Double number1 = input.nextDouble();  // read first number
 
-        System.out.print("Enter second number : ");   // prompt user to input second number
+        System.out.print("Enter second number is : ");   // prompt user to input second number
         Double number2 = input.nextDouble();  // read second number
 
         Double square1 = number1 * number1;   // calculate square of the first number
         Double square2 = number2 * number2;   // calculate square of the second number
 
-        System.out.printf("Square of first number : %f%n", square1);  // print square of the first number
-        System.out.printf("Square of second number : %f%n", square2);   // print square of the second number
+        System.out.printf("Square of first number is : %f%n", square1);  // print square of the first number
+        System.out.printf("Square of second number is : %f%n", square2);   // print square of the second number
 
         Double sum = square1 + square2;   // calculate the sum of the square of number 1 and number 2
-        System.out.printf("Sum of their square : %f%n", sum); // print the sum
+        System.out.printf("Sum of their square is : %f%n", sum); // print the sum
 
         Double difference = square1 - square2;   // calculate the difference of the square of number 1 and number 2
-        System.out.printf("Difference of their square : %f%n", difference);   // print the difference
+        System.out.printf("Difference of their square is : %f%n", difference);   // print the difference
     }
 
 }
